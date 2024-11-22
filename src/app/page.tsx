dd1ee8587aa5cc815c44c6c1c80e8c18986940c0
Luke Shiels <47612057+skve@users.noreply.github.com>
2024-11-21 19:03:18 -0800

    page
    Signed-off-by: Luke Shiels <47612057+skve@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,37 +30,68 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <header className="px-3 bg-sand-2 py-2.5 border-b border-sand-5 ">
+    <div className="min-h-screen flex flex-col">
+      <header className="px-3 bg-sand-1 py-2.5 border-b border-sand-5">
         <div className="flex gap-2 items-center text-teal-12">
           <ShadowIcon className="size-6" />
           <h1 className="text-2xl font-serif font-bold">Detour Dynamics</h1>
         </div>
       </header>
 
-      <nav className="bg-sand-1 border-b border-sand-5 px-3 py-2.5">
-        <button
-          disabled={pending}
-          onClick={() => startTransition(fetchRecommendations)}
-          className="bg-tomato-3 border border-tomato-6 text-tomato-9 hover:bg-tomato-4 transition font-medium py-2 px-4 rounded-xl flex gap-2 items-center"
-        >
-          {pending ? (
-            <LightningBoltIcon className="size-4 animate-pulse" />
-          ) : (
-            <SewingPinIcon className="size-4" />
-          )}
-          Fetch Airport Recommendations{" "}
-        </button>
-      </nav>
+      <div className="flex flex-1">
+        <aside className="min-w-80 border-r border-sand-5 bg-sand-1 py-4 px-6 space-y-4">
+          <div>
+            <h2 className="text-lg font-medium mb-2">Current Flight Status</h2>
+            <dl className="space-y-2">
+              <div>
+                <dt className="text-sand-11 font-medium text-sm font-mono uppercase">POSITION</dt>
+                <dd className="tabular-nums">
+                  {flightState.currentPosition.latitude.toFixed(4)}°N,{" "}
+                  {flightState.currentPosition.longitude.toFixed(4)}°W
+                </dd>
+              </div>
+              <div>
+                <dt className="text-sand-11 font-medium text-sm font-mono uppercase">REMAINING FUEL</dt>
+                <dd className="tabular-nums">{flightState.remainingFuel.toLocaleString()} kg</dd>
+              </div>
+              <div>
+                <dt className="text-sand-11 font-medium text-sm font-mono uppercase">AIRCRAFT</dt>
+                <dd className="tabular-nums">{flightState.airplaneModel.type}</dd>
+              </div>
+              <div>
+                <dt className="text-sand-11 font-medium text-sm font-mono uppercase">RANGE REMAIN</dt>
+                <dd className="tabular-nums">{flightState.airplaneModel.range.toLocaleString()} km</dd>
+              </div>
+            </dl>
+          </div>
 
-      <main className="px-3 grid grid-cols-1 md:grid-cols-2 gap-4 space-y-4 py-2.5">
-        {recommendations.map((airport) => (
-          <AirportRecommendation
-            key={airport.icao}
-            airport={airport as AirportInfo & { distance: number }}
-          />
-        ))}
-      </main>
+          <div>
+            <button
+              disabled={pending}
+              onClick={() => startTransition(fetchRecommendations)}
+              className="w-full bg-tomato-3 border border-tomato-6 text-tomato-9 hover:bg-tomato-4 transition font-medium py-2 px-4 rounded-xl flex gap-2 items-center justify-center"
+            >
+              {pending ? (
+                <LightningBoltIcon className="size-4 animate-pulse" />
+              ) : (
+                <SewingPinIcon className="size-4" />
+              )}
+              Find Airports
+            </button>
+          </div>
+        </aside>
+
+        <div className="flex-1 bg-sand-2">
+          <main className="flex-1 bg-sand-2 p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+            {recommendations.map((airport) => (
+              <AirportRecommendation
+                key={airport.icao}
+                airport={airport as AirportInfo & { distance: number }}
+              />
+            ))}
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
